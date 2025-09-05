b1821cdc53e32e30aced4109c8a5854d9da30e07
RESU SAINANDHU <sainandhunaiduresu@gmail.com>
2025-09-05 21:42:18 +0530

    Create 2.4rightTriangleHallow.java

---
+++ 2.4rightTriangleHallow.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main (String...args) {
+        int i , j ;
+        int n = 5 ;
+     
+        for(i = 1 ; i <= n ; i++ ) {
+            for(j = 1 ; j <= i ; j++) {
+                if(j==1 || i==5 || i==j){
+                    System.out.print("*");
+                } else {
+                   System.out.print(" ") ;
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
