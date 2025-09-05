891be0dd4734d4abf786ff0134d81072e2289c99
RESU SAINANDHU <sainandhunaiduresu@gmail.com>
2025-09-05 21:16:24 +0530

    Create 1.1pattern.java

---
+++ 1.1pattern.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String...args) {
+        int i , j ;
+        int n = 5 ;
+        for(i = 1 ; i <= n ; i++ ) {
+            for(j = 1 ; j <= n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
